3a1c2cfe3998322a01f9bc3da171d958870aeb7d
Carol <acsb@cin.ufpe.br>
2018-09-29 03:25:04 -0300

    corrigindo o app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { FormsModule, ReactiveFormsModule, FormControl} from '@angular/forms';
-=======
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
->>>>>>> master
 
 import {ROUTES} from './app.routes';
 
@@ -19,11 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
 import { GraphsComponent } from './graphs/graphs.component';
 import { Graphs2Component } from './graphs2/graphs2.component';
-<<<<<<< HEAD
-import { MonthDatePickerComponent } from './month-date-picker/month-date-picker.component';
-=======
 import { PyramidGraphicComponent } from './graphs/pyramid-graphic/pyramid-graphic.component';
->>>>>>> master
 
 
 @NgModule({
@@ -33,24 +25,15 @@ import { PyramidGraphicComponent } from './graphs/pyramid-graphic/pyramid-graphi
     InfoCardsComponent,
     GraphsComponent,
     Graphs2Component,
-<<<<<<< HEAD
-    MonthDatePickerComponent
-=======
     PyramidGraphicComponent
->>>>>>> master
   ],
   imports: [
     BrowserModule,
     NgbModule,
     HttpClientModule,
-<<<<<<< HEAD
     FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
-=======
     RouterModule.forRoot(ROUTES),
     FormsModule
->>>>>>> master
   ],
   providers: [DataService],
   bootstrap: [AppComponent]

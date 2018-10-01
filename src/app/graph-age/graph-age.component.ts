7d013ffe168718ecb100f93345d066519f4c89e0
Marco Santana <m.aurelio_santana@hotmail.com>
2018-10-01 11:47:48 -0300

    grafico funcionando e finalizado

--- src/app/graph-age/graph-age.component.ts
+++ src/app/graph-age/graph-age.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../data.service';
 import { Chart } from 'chart.js';
+import { EMPTY } from '../../../node_modules/rxjs';
+import { $ } from '../../../node_modules/protractor';
 
 @Component({
   selector: 'db-graph-age',
@@ -20,8 +22,7 @@ export class GraphAgeComponent implements OnInit {
   anosIniciais = []
   anosFinais = []
   idadesDisp = []
-  firstTime  = true;
-  label = [];
+  label= [];
   datas =[];
 
   startYears(){
@@ -35,11 +36,12 @@ export class GraphAgeComponent implements OnInit {
   }
 
   controlSubmit(form){//pega os dados e passa para ngOnInit
+    this.chart.destroy();//quebra o grafico antigo.
     this.control.anoInicial = parseInt(form.value.anoInicial,10);
     this.control.anoFinal = parseInt(form.value.anoFinal, 10);
     this.control.age = parseInt(form.value.age, 10);
-    console.log('AQUI AAA');
     this.ngOnInit();
+
   }
 
   constructor(private _brazil: DataService) {  };
@@ -52,62 +54,51 @@ export class GraphAgeComponent implements OnInit {
     let anoFinal = this.control.anoFinal;
     anoFinal -= 1950;//Normalize
     let age = this.control.age;
-    console.log('age', age);
-    console.log('final ', anoFinal);
-    console.log('inic ', anoInicial);
+
+    this.label=[];
+    this.datas=[];
     this._brazil.populacaoLimitada(age).subscribe(res => {
-        console.log('ENKTROU');
         request = res;
         //Aqui o index é o ano (sendo 1950 = 0 e 2000=50 e etc. Dessa forma: ANO = index+1950)
-        this.label = [];
-        this.datas =[];
         for(var i=anoInicial; i<=anoFinal;i++){
           //Consulta por ano.
-          this.label.push(request[i]['year']);
-          this.datas.push(request[i]['total'].toString());
+          // console.log(request[i]['year'] ,' y - total', request[i]['total']);
+          this.label.push(request[i]['year'])
+          this.datas.push({y:request[i]['total'].toString(), x:request[i]['year']});
         }
-        if(this.firstTime) {
-          this.chart = new Chart('canvasAge', {
+        
+        this.chart = new Chart('canvasAge', {
           //Resolver aqui as coisas do grafico.
-            type:'line',
-            data: {
-              labels:this.label,
-              datasets: [{
-                  data: this.datas,
-                  label:'População',
-                  borderColor: 'rgba(186, 70, 70, 0.6)',
-                  backgroundColor:'rgba(186, 70, 70, 0.2)',
-                  fill:true,
-                  showLine: true,
-              }]
+          type:'line',
+          data: {
+            labels:this.label,
+            datasets: [{
+                data: this.datas,
+                label:'População',
+                borderColor: 'rgba(186, 70, 70, 0.6)',
+                backgroundColor:'rgba(186, 70, 70, 0.2)',
+                fill:true,
+                showLine: true,
+            }]
+          },
+          options:{
+            title:{
+              display: true,
+              text: 'População brasileira com '+age.toString()+ ' entre '+ (anoInicial+1950).toString() + ' e '+ (anoFinal+1950).toString(),
             },
-            options:{
-              title:{
-                display: true,
-                text: 'População brasileira com '+age.toString()+ ' entre '+ (anoInicial+1950).toString() + ' e '+ (anoFinal+1950).toString(),
-              },
-              showLines: true, // disable for all datasets
-              legend:{
-              },
-              scales: {
-                xAxes: [{
-                  display: true
-                }],
-                yAxes: [{
-                  display: true
-                }]
-              }
+            showLines: true, // disable for all datasets
+            legend:{
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }]
             }
-          });
-          this.firstTime = false;
-        }
-        else{
-          this.chart.data.datasets[0].data = this.datas;
-          console.log(this.chart.data.datasets[0].label);
-          console.log('AQUI');
-          this.chart.options.title.text = 'População brasileira com '+age.toString()+ ' entre '+ (anoInicial+1950).toString() + ' e '+ (anoFinal+1950).toString();
-          this.chart.update();
-        }
+          }
+        });
       });
     };
 }
